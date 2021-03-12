795c7b66666103f4ef716e2fe2fde27a54ff91f6
Adam Chlebek <adamchlebek@live.com>
2021-03-11 17:14:40 -0700

    Finished Project

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h2>Personal Info</h2>
+    Name: {{ myInfo.name }}
+    <br/>
+    Semester: {{ myInfo.semester }} {{ myInfo.year }}
+    <br/>
+
+    Courses
+    <ol>
+      <li v-for="(c,pos) in myInfo.courses" :key="pos">
+        {{ c.name }} ({{ c.credits }} credits)
+      </li>
+    </ol>
+
+    <table>
+      <tr>
+        <th>Course</th>
+        <th>Credits</th>
+      </tr>
+      <tr v-for="(c,pos) in myInfo.courses" :key="pos">
+        <td>{{ c.name }}</td>
+        <td class="center">{{ c.credits }}</td>
+      </tr>
+    </table>
+
+    <CourseCard 
+      v-for="(c,pos) in myInfo.courses" 
+      :key="pos" 
+      v-bind:name="c.name" 
+      v-bind:num-credits="c.credits"
+      v-bind:meetings="c.meetings"
+    ></CourseCard>
+    <!-- <hello-world msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
+import CourseCard from './components/CourseCard.vue';
 
 @Component({
   components: {
     HelloWorld,
+    CourseCard,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  readonly myInfo = {
+    name: "Adam Chlebek",
+    semester: "Winter",
+    year: 2021,
+    courses: [
+      {
+        name: "CIS 371 Web Application Programming",
+        credits: 3,
+        meetings: ["Mon 12p-12:50p", "Wed 12p-12:50p", "Fri 12p-12:50p"]
+      },
+      {
+        name: "CIS 452 Operating Systems Concepts",
+        credits: 4,
+        meetings: ["Thu 12p-1:50p"]
+      },
+      {
+        name: "CIS 467 Computer Science Project",
+        credits: 4,
+        meetings: ["Tue 6a-7:50a", "Thu 6a-7:50a"]
+      },
+      {
+        name: "GEO 111 Exploring The Earth",
+        credits: 4,
+        meetings: ["None"]
+      }
+    ]
+  };
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  table, th, td {
+    border: 1px solid black;
+  }
+
+  .center {
+    text-align: center;
+  }
 </style>
---
+++ src/components/CourseCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="crs">
+    <h3>Course: {{ name }} ({{ numCredits }} credits)</h3>
+    <br/>
+    <h4>Meetings at: </h4>
+    <p v-for="(t,pos) in meetings" :key="pos">{{ t }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from "vue-property-decorator";
+
+    @Component
+    export default class CourseCard extends Vue {
+        @Prop() readonly name!: string;
+        @Prop() readonly numCredits!: number;
+        @Prop() readonly meetings!: string[];
+    }
+</script>
+
+<style>
+#crs {
+  display: inline-block;
+  border: 2px solid hsl(30, 90%, 50%);
+  border-radius: 0.5em;
+  padding: 1em;
+  margin: 0.6em;
+  box-shadow: 3px 5px 6px 7px hsla(30, 55%, 50%, 0.4);
+}
+</style>
